f2d83f8d8faca32f2d539c92bdae246b8f7781a0
Richard Lee <14349+dlackty@users.noreply.github.com>
2021-05-05 02:04:05 +0800

    Initialize the project

---
+++ lib/uid2.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "uid2/version"
+
+module Uid2
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/uid2_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Uid2 do
+  it "has a version number" do
+    expect(Uid2::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
