51ec1f4ce93eabaca27666e35e6f69f3798578f5
Richard Lee <14349+dlackty@users.noreply.github.com>
2021-05-07 13:47:26 +0800

    Minor style fixes

--- lib/uid2.rb
+++ lib/uid2.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
 require_relative "uid2/version"
-require_relative 'uid2/client'
+require_relative "uid2/client"
--- lib/uid2/client.rb
+++ lib/uid2/client.rb
@@ -1,78 +1,81 @@
-require 'net/http/persistent'
-require 'faraday'
-require 'faraday_middleware'
-require 'time'
+# frozen_string_literal: true
+
+require "net/http/persistent"
+require "faraday"
+require "faraday_middleware"
+require "time"
 
 module Uid2
   class Client
     attr_accessor :bearer_token, :base_url
 
-    def initialize(options = {})
+    def initialize(_options = {})
       yield(self) if block_given?
 
-      self.base_url ||= 'https://integ.uidapi.com/v1/'
+      self.base_url ||= "https://integ.uidapi.com/v1/"
     end
 
     def generate_token(email: nil, email_hash: nil)
-      raise ArgumentError, 'Either email or email_hash needs to be provided' if email.nil? && email_hash.nil?
+      raise ArgumentError, "Either email or email_hash needs to be provided" if email.nil? && email_hash.nil?
 
       # As stated in doc, if email and email_hash are both supplied in the same request,
       # only the email will return a mapping response.
-      params = unless email.empty?
-                 { email: email }
-               else
+      params = if email.empty?
                  { email_hash: email_hash }
+               else
+                 { email: email }
                end
-      http.get('token/generate', params).body
+      http.get("token/generate", params).body
     end
 
     def validate_token(token:, email: nil, email_hash: nil)
-      raise ArgumentError, 'Either email or email_hash needs to be provided' if email.nil? && email_hash.nil?
-      params = unless email.empty?
-                 { email: email }
-               else
+      raise ArgumentError, "Either email or email_hash needs to be provided" if email.nil? && email_hash.nil?
+
+      params = if email.empty?
                  { email_hash: email_hash }
+               else
+                 { email: email }
                end
 
-      http.get('token/validate', params.merge(token: token)).body
+      http.get("token/validate", params.merge(token: token)).body
     end
 
     def refresh_token(refresh_token:)
-      http.get('token/refresh', { refresh_token: refresh_token }).body
+      http.get("token/refresh", { refresh_token: refresh_token }).body
     end
 
     def get_salt_buckets(since: Time.now)
       # By default, Ruby's iso8601 generates timezone parts (`T`)
       # which needs to be removed for UID2 APIs
-      http.get('identity/buckets', since_timestamp: since.utc.iso8601[0..-2]).body
+      http.get("identity/buckets", since_timestamp: since.utc.iso8601[0..-2]).body
     end
 
     def generate_identifier(email: nil, email_hash: nil)
-      raise ArgumentError, 'Either email or email_hash needs to be provided' if email.nil? && email_hash.nil?
+      raise ArgumentError, "Either email or email_hash needs to be provided" if email.nil? && email_hash.nil?
 
       # As stated in doc, if email and email_hash are both supplied in the same request,
       # only the email will return a mapping response.
-      params = unless email.empty?
-                 { email: email }
-               else
+      params = if email.empty?
                  { email_hash: email_hash }
+               else
+                 { email: email }
                end
 
-      http.get('identity/map', params).body
+      http.get("identity/map", params).body
     end
 
     def batch_generate_identifier(email: nil, email_hash: nil)
-      raise ArgumentError, 'Either email or email_hash needs to be provided' if email.nil? && email_hash.nil?
+      raise ArgumentError, "Either email or email_hash needs to be provided" if email.nil? && email_hash.nil?
 
       # As stated in doc, if email and email_hash are both supplied in the same request,
       # only the email will return a mapping response.
-      params = unless email.empty?
-                 { email: Array(email) }
-               else
+      params = if email.empty?
                  { email_hash: Array(email_hash) }
+               else
+                 { email: Array(email) }
                end
 
-      http.post('identity/map', params).body
+      http.post("identity/map", params).body
     end
 
     private
